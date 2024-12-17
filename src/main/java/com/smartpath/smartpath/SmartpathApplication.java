e7c68c85b5ef735e7e900533cc0ca8344016addb
Raj kumar Davu <147226633+Srirajkumar3@users.noreply.github.com>
2024-12-17 13:53:33 +0530

    Initial commit

---
+++ src/main/java/com/smartpath/smartpath/SmartpathApplication.java
@@ -0,0 +1,13 @@
+package com.smartpath.smartpath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartpathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartpathApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartpath/smartpath/SmartpathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartpath.smartpath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartpathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
